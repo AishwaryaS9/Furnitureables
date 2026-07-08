f14cee9cbbec57d49a9ed545a632142a927c55e8
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2026-07-08 17:13:11 +0530

    feat: Sync webhook with prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,26 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
-export const prisma = new PrismaClient({
+const createPrismaClient = () =>
+  new PrismaClient({
     adapter: new PrismaPg(pool),
-});
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
+  });
+
+export const prisma =
+  global.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
