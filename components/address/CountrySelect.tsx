7ffe5cd8afa1f0a4b37d2ffcdda40469195a6881
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2026-07-20 10:05:46 +0530

    feat(address): add country flags to country and phone code selectors

--- components/address/CountrySelect.tsx
+++ components/address/CountrySelect.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { Country } from "country-state-city";
+import ReactCountryFlag from "react-country-flag";
 import { cn } from "@/lib/utils";
 
 import {
@@ -47,9 +48,22 @@ export default function CountrySelect({
             onOpenChange={setOpen}
         >
             <PopoverTrigger className="flex w-full items-center justify-between rounded-md border px-3 py-2 text-sm">
-                {selectedCountry
-                    ? `${selectedCountry.flag} ${selectedCountry.name}`
-                    : "Select country"}
+                <div className="flex items-center gap-2">
+                    {selectedCountry && (
+                        <ReactCountryFlag
+                            countryCode={selectedCountry.isoCode}
+                            svg
+                            style={{
+                                width: "1.25rem",
+                                height: "1.25rem",
+                            }}
+                        />
+                    )}
+
+                    <span>
+                        {selectedCountry?.name ?? "Select Country"}
+                    </span>
+                </div>
                 <ChevronsUpDown className="h-4 w-4 opacity-50" />
             </PopoverTrigger>
             <PopoverContent
@@ -83,12 +97,18 @@ export default function CountrySelect({
                                                 : "opacity-0"
                                         )}
                                     />
+                                    <div className="flex items-center gap-3">
+                                        <ReactCountryFlag
+                                            countryCode={country.isoCode}
+                                            svg
+                                            style={{
+                                                width: "1.25rem",
+                                                height: "1.25rem",
+                                            }}
+                                        />
 
-                                    <span className="mr-2">
-                                        {country.flag}
-                                    </span>
-
-                                    {country.name}
+                                        <span>{country.name}</span>
+                                    </div>
                                 </CommandItem>
                             ))}
                         </CommandGroup>
